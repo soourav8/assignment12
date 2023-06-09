74c27794535cfe30af7ae32fcf96690f3ebde18c
soourav8 <soourav8@gmail.com>
2023-06-09 22:14:37 +0530

    router setup done

---
+++ src/Route/Route.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
