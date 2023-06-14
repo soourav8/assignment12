20920e7a53017e6f24b35446705921b11280d96f
soourav8 <soouav8@gmail.com>
2023-06-14 11:20:06 +0530

    instructor shows done

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { InstructorsCard } from './InstrucrorsCard';
 
 const Instructor = () => {
+    const [instructors, setInstructors] = useState([]);
+    useEffect( ()=>{
+        fetch("http://localhost:5000/instructors")
+        .then(res => res.json())
+        .then(data => setInstructors(data))
+    },[])
+
     return (
-        <div>
-            <h2>This is from instructor</h2>
+        <div className=' mt-12 mb-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
+            {
+                instructors.map(item => <InstructorsCard key={item._id} item={item}></InstructorsCard>)
+            }
+            
         </div>
     );
 };
