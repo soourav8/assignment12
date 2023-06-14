ac02c113f1b94157ca447d40c5a181a683d8654a
soourav8 <soouav8@gmail.com>
2023-06-14 13:49:30 +0530

    button disable and card bg red done

--- src/Pages/Classes/ClassesCard.jsx
+++ src/Pages/Classes/ClassesCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 export const ClassesCard = ({item}) => {
+    const cardStyle = { backgroundColor : item.availableSeats == 0 ? "red" : "inherit"}
+    const buttonDisabled = item.availableSeats === 0;
     return (
         <div className="-view">
-            <div className="card w-75 h-full bg-base-100 shadow-xl">
+            <div style={cardStyle} className="card w-75 h-full bg-base-100 shadow-xl">
                 <figure><img src={item.image} /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{item.name}</h2>
                     <p> Name: {item.name}</p>
                     <p>Instructor name: {item.instructorName}</p>
-                    <p>Available seats: {item.availableSeats}</p>
+                    <div>
+                    
+                    <p >Available seats: {item.availableSeats}</p>
+                    </div>
                     <p>Price: {item.price}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button disabled={buttonDisabled} className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
